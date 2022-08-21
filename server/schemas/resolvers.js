04cec0f62853526914123a7720413d7763f4fc10
James Pritchard <james.edward.pritchard@gmail.com>
2022-08-21 14:29:40 -0400

    MERNG template

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,51 @@
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
+
+
+
+const resolvers = {
+  Query: {
+    //USER
+    //Single User
+    user: async (parent, { _id }, context)=> {  
+      if (context.user) {
+      return User.findOne({
+          _id: context.user._id 
+          });
+      }
+      throw new AuthenticationError('You need to be logged in! resolvers');
+  
+    }
+  },
+  Mutation: {
+    //USER
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    }
+
+    }
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,31 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+
+
+
+
+type Query{
+    user: User
+}
+type Mutation {
+    # USER
+    addUser(username: String!, email: String! password: String!): Auth
+    login(email: String!, password: String!): Auth
+}
+`;
+
+
+
+module.exports = typeDefs;
