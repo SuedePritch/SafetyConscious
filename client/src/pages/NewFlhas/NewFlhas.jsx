60fa370f2dfb9ce13f69424b6b95c482dc41f89e
James Pritchard <james.edward.pritchard@gmail.com>
2022-09-09 19:03:03 -0400

    newFlha styling

--- client/src/pages/NewFlhas/NewFlhas.jsx
+++ client/src/pages/NewFlhas/NewFlhas.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './NewFlhas.scss'
 import { useQuery} from '@apollo/client';
 import {GET_FLHAS} from '../../utils/queries';
 
@@ -10,28 +11,31 @@ function NewFlhas() {
     if (error) return `Error! ${error.message}`;
     if(!loading && !error){
     flhas = data.allFLHAs
-    
     }
     return(
         <div>
             <Navbar/>
-            <h2>New FLHAS</h2>
         {flhas.map((flha) => {
+            // If created within the last 24 hours (86400000 miliseconds)
             if(Date.now() - flha.dateCreated < 86400000){
-        return    <div className="main-content" key={flha._id}>
-                <p>{flha.jobLocation}</p>
-                <p>{flha.supervisor}</p>
-                <p>{flha.primarytask}</p>
-                {flha.jobTask.map((task)=>{
-                    //If created within the last 24 hours(86400000 miliseconds) 
+        return <div className="single-flha" key={flha._id}>
+                    <div className='flha-details'>
+                        <p>Location: {flha.jobLocation}</p>
+                        <p>Supervisor: {flha.supervisor}</p>
+                        <p>Job Task: {flha.primarytask}</p>
+                    </div>
+                        <div className='flha-buttons'>
+                            <button>Edit</button>
+                            <button>Good</button>
+                        </div>
+                    {flha.jobTask.map((task)=>{
                     
-                        return  <div key={task._id}>
-                                <p>{task.task}</p>
-                                <p>{task.hazard}</p>
-                                <p>{task.control}</p>
-                                <hr />
-                            </div>
-                })}
+                            return  <div className='job-task' key={task._id}>
+                                    <p><b>Task:</b> {task.task}</p>
+                                    <p><b>Hazard:</b> {task.hazard}</p>
+                                    <p><b>Control:</b> {task.control}</p>
+                                </div>
+                    })}
             </div>
             }else{return <div key={Math.random()}></div>}
 
